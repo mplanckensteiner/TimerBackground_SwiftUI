1f6737a21afd8da5685e0ea7af96a24ccdf864fd
Miguel Planckensteiner <49713385+mikeplanck@users.noreply.github.com>
2020-05-12 15:08:16 +0200

    Initial Commit

---
+++ TimerBackground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimerBackground
+//
+//  Created by Miguel Planckensteiner on 12.05.20.
+//  Copyright © 2020 Miguel Planckensteiner. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
